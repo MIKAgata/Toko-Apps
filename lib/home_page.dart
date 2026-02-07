50063ad82e340dfa6cbef2d80662b6a526d927ed
MIKE <mikael.agata.sitepu@gmail.com>
2026-02-07 21:29:08 +0700

    update fitur halaman navi

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,68 +4,72 @@ import 'checkin_page.dart';
 import 'setting.dart';
 import 'toko.dart';
 
-
-class HomePage extends StatelessWidget {
+/// Main HomePage dengan Bottom Navigation
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  // List screens untuk IndexedStack
+  late final List<Widget> _screens;
+
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      const Toko(), // Home/Toko
+      const CheckInPage(), // CheckIn/Keranjang
+      const ProfilePage(), // Profile
+      const Settingpage(), // Setting
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: const Text('Profile'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ProfilePage()),
-                );
-              },
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              child: const Text('CheckIn'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CheckInPage()),
-                );
-              },
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              child: const Text('Setting'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Settingpage()),
-                );
-              },
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              child: const Text('Toko'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Toko()),
-                );
-              },
-            ),
-            // const SizedBox(height: 10),
-            // ElevatedButton(
-            //   child: const Text('Back'),
-            //   onPressed: () {
-            //     Navigator.pop(context);
-            //   },
-            // ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(0xFF00AA5B),
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            activeIcon: Icon(Icons.shopping_cart),
+            label: 'Shop',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_outlined),
+            activeIcon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ZERO Company',
+      title: 'ZERO Store',
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
